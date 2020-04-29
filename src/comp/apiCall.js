518a5e4358bf005bb58b5a1e394ea431ad1c7bfe
MeirOverferst <meiroverferst@gmail.com>
2020-04-29 18:25:04 +0200

    1 per mil cases render with color

--- src/comp/apiCall.js
+++ src/comp/apiCall.js
@@ -3,32 +3,54 @@ import { useState } from 'react'
 
 
 
-export const Fetcher=()=>{
+// export const Fetcher=()=>{
 
-  const [hasError, setErrors] = useState(null);
-  const [results, setResults] = useState(" ");
+//   const [hasError, setErrors] = useState(null);
+//   const [results, setResults] = useState(" ");
 
  
 
-  fetch("https://api.covid19api.com/summary")
-  .then(res => res.json())
-  .then(json => {
-    if (json) {
-      setResults(json)
-    } else {
-      setResults(" ")
-    }
-  })
-  .catch(err => {
-    setErrors(err)
+//   fetch("https://api.covid19api.com/summary")
+//   .then(res => res.json())
+//   .then(json => {
+//     if (json) {
+//       setResults(json)
+//     } else {
+//       setResults(" ")
+//     }
+//   })
+//   .catch(err => {
+//     setErrors(err)
   
-  })
-console.log("results",results);
+//   })
+// console.log("results",results);
 
-  return {hasError,results}
-}
+//   return {hasError,results}
+// }
 
 
+export async function fetchData() {
+    const [hasError, setErrors] = useState(false);
+  const [results, setResults] = useState(" ");
+  try {
+      const requestOptions = {
+          method: 'GET',
+          redirect: 'follow'
+        };    
+      const response = await fetch("https://api.covid19api.com/summary", requestOptions);
+      const json = await response.json();
+      const countries =json.Countries;
+      // console.log(countries);
+
+      setResults(countries);
+    } catch (error) {
+      setErrors(error);
+    }
+    console.log("Has error:", JSON.stringify(hasError));    
+           console.log(results);
+
+   
+  }
 
 
 
--- src/comp/map.js
+++ src/comp/map.js
@@ -3,17 +3,48 @@ import {ComposableMap,ZoomableGlobe,Geographies,Geography} from "react-simple-ma
 import { Motion, spring } from "react-motion";
 import Data from "../data/110m.json";
 import ReactTooltip from "react-tooltip";
-
-// console.log(results) ;
-
-const mapStyles = {
-    width: "50%",
-    height: "auto",
-  };
+import { scaleLinear } from "d3-scale";
+// import cBp from "../data/country-by-population.json";
 
 
-const Map =({fetchData})=>{
+const mapStyles = { width: "50%",height: "auto"};
 
+const colorScale = scaleLinear()
+  // .domain([0,1000]);
+  .domain([0,1200])
+  .range(["#95ffff","#6bfaff", "#55c8cc", "#409699","#204b4c"]);
+  // .range(["#ffedea", "#ff5233"]);
+
+const Map =()=>{
+  ///////API CALL
+  const [hasError, setErrors] = useState(false);
+  const [results, setResults] = useState([ ]);
+
+  useEffect(() => {
+    async function fetchData() {
+
+    try {
+      const requestOptions = {
+          method: 'GET',
+          redirect: 'follow'
+        };    
+      const response = await fetch("https://api.covid19api.com/summary", requestOptions);
+      const json = await response.json();
+      const countries =json.Countries;
+      setResults(countries);
+      if(countries){
+         }
+ 
+    } catch (error) {
+      setErrors(error);
+    }
+    console.log("Has error:", JSON.stringify(hasError));  
+  }
+  fetchData();
+
+  },[]);
+
+  // let cBps =cBp.find(r => r.country === results.properties.NAME_LONG);
 
 const [turn,setTurn]=useState(1);
 const [center,setCenter]=useState([0, 0]);
@@ -25,7 +56,7 @@ const LifecycleDemo=()=>{
 useEffect(() => {
 let interval = setTimeout(() => {
 setCenter([center[0]+turn,center[1]]);
-}, 1);
+}, 10);
 return () => clearTimeout(interval);
   },[]);
 return "mounted";
@@ -54,11 +85,13 @@ const circleProps={
   stroke:"#A0B2A6",
   strokeWidth: 0.1,
 };
+
+
+
       return (
         <div data-tip="" style={{ textAlign: "center"  }}>
-        <div  style={{display: "none"}} >{mounted && <LifecycleDemo />}</div>
+        <div style={{ display: "none"  }} >{mounted && <LifecycleDemo />}</div>
         <button className="btn" onClick={oneForTwo}>{"Toggle Spin"}</button>
-        <button className="btn" onClick={fetchData}>Fetch Data</button>
         <button className="btn" onClick={changeCenter([34.7818, 32.0853])}>{"Tel Aviv"}</button>
         <button className="btn" onClick={changeCenter([ 9.9937, 53.5511])}>{"Hamburg"}</button>
         <ReactTooltip  place="top" type="light" effect="float">
@@ -79,13 +112,17 @@ const circleProps={
           <ComposableMap  width={500} height={500} projection="orthographic" projectionConfig={{ scale: 175 }} style={mapStyles}>        
             <ZoomableGlobe  center={[x, y]} >
             <circle style={circleProps}/>
+            {results.length>0 &&( 
                 <Geographies disableOptimization geography={Data}>
                 {(geographies,proj) =>(       
-                  geographies.map((geography,i) => (
+                  geographies.map((geography,i) => {    
+                     let d = results.find(s => s.CountryCode === geography.properties.ISO_A2);
+                          return(
                       <Geography
                         key={i}
                         geography={geography}
                         projection={proj}
+                        fill={d ? colorScale( (d["TotalConfirmed"]/geography.properties.population)*(1000000))  : "#F5F4F6"}
                         onMouseOver={() => {
                            setContent(geography.properties.NAME);
                         }}
@@ -97,8 +134,9 @@ const circleProps={
                         }}
                         style={
                           {
+                            ///default fill
                           default: {
-                            fill:"#CFD8DC",
+                           
                             stroke: "#33329e",
                             strokeWidth: 0.10,
                             outline: "none",
@@ -116,10 +154,15 @@ const circleProps={
                             outline: "none"
                           },
                         }}
-                        >                    
-                      </Geography>                    
-                    )))}               
+                        />                    
+                      )                   
+                      }
+                    
+                    ))}               
                     </Geographies>
+
+                    )}
+
               </ZoomableGlobe>
             </ComposableMap>
           )}
@@ -132,3 +175,5 @@ const circleProps={
 
 
 export default Map;
+
+// <button className="btn" onClick={fetchData}>Fetch Data</button>
